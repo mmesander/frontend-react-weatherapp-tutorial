993fe504ea7267510ac7c05da7b1e90217591e8e
mmesander <mark_mesander@hotmail.com>
2023-05-29 17:28:37 +0200

    De gegevens zichtbaar gemaakt indien er ook een request ingediend is, daarnaast wat formatting gedaan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //Functions
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import axios from "axios";
 
@@ -11,47 +11,57 @@ import MetricSlider from './components/metricSlider/MetricSlider';
 const apiKey = '08ebcec99a4487212029dd95f36fa8de';
 
 function App() {
-  async function fetchData() {
-    try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
-      const weatherData = response.data;
-      console.log(weatherData);
-    } catch (e) {
-      console.error(e);
+    const [weatherData, setWeatherData] = useState({});
+
+    async function fetchData() {
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+            console.log(response.data);
+            setWeatherData(response.data);
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
-  return (
-    <>
-      <div className="weather-container">
 
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar/>
+    return (
+        <>
+            <div className="weather-container">
 
-          <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+                {/*HEADER -------------------- */}
+                <div className="weather-header">
+                    <SearchBar/>
 
-            <button type="button">
+                    <span className="location-details">
+                        {Object.keys(weatherData).length > 0 &&
+                            <>
+                                <h2>{weatherData.weather[0].description}</h2>
+                                <h3>{weatherData.name}</h3>
+                                <h1>{weatherData.main.temp}</h1>
+                            </>
+                        }
+
+                        <button
+                            type="button"
+                            onClick={fetchData}
+                        >
               Haal data op!
             </button>
           </span>
-        </div>
+                </div>
 
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu/>
+                {/*CONTENT ------------------ */}
+                <div className="weather-content">
+                    <TabBarMenu/>
 
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
-          </div>
-        </div>
+                    <div className="tab-wrapper">
+                        Alle inhoud van de tabbladen komt hier!
+                    </div>
+                </div>
 
-        <MetricSlider/>
-      </div>
-    </>
-  );
+                <MetricSlider/>
+            </div>
+        </>
+    );
 }
 
 export default App;
