70a0024783383e2b5ab485ac4ea884b9d6210711
mmesander <mark_mesander@hotmail.com>
2023-05-30 13:44:29 +0200

    asynchrone functie gemaakt die gegevens over de coordinatie ophaalt

--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -1,11 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import './ForecastTab.css';
+import axios from "axios";
 
 function ForecastTab({coordinates, key}) {
     const [forecasts, setForecasts] = useState([]);
 
     useEffect(() => {
-    }, [coordinates])
+        async function fetchForecasts() {
+            try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.long}&appid=${key}&lang=nl`);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }, [coordinates]);
 
     return (
     <div className="tab-wrapper">
