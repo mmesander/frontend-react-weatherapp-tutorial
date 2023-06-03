01895a5671f84f3a6f1340119c9cf18b9f1e79d0
mmesander <mark_mesander@hotmail.com>
2023-06-03 14:48:11 +0200

    create function toggleTemp

--- src/context/TempProvider.js
+++ src/context/TempProvider.js
@@ -1,8 +1,18 @@
-import React, {createContext} from "react";
+import React, {createContext, useState} from "react";
 
 export const TempContext = createContext(null);
 
 function TempContextProvider({children}) {
+    const [selectedMetric, setSelectedMetric] = useState('celcius');
+
+    function toggleTemp() {
+        if (selectedMetric === 'celcius') {
+            setSelectedMetric('fahrenheit');
+        } else {
+            setSelectedMetric('celcius');
+        }
+    }
+
     return (
         <TempContext.Provider value={}>
             {children}
