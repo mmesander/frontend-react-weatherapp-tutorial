80ebdb10e60c4536989df589f6a3ca0fb2bf108e
mmesander <mark_mesander@hotmail.com>
2023-06-03 13:26:45 +0200

    add parameters in function WeatherDetail

--- src/components/weatherDetail/WeatherDetail.js
+++ src/components/weatherDetail/WeatherDetail.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './WeatherDetail.css';
+import kelvinToCelsius from "../../helpers/kelvinToCelsius";
 
-function WeatherDetail() {
+function WeatherDetail({temp, type, description}) {
   return (
     <section className="day-part">
       <span className="icon-wrapper">
         *Icoontje van het weer*
       </span>
-      <p className="description">Zonnig</p>
-      <p>13&deg; C</p>
+      <p className="description">{description}</p>
+      <p>{kelvinToCelsius(temp)}</p>
     </section>
   );
 }
